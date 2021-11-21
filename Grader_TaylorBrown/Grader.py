76e9b234e656998c3b580b543aff6cced70cbbca
TaylorBrown96 <78773029+TaylorBrown96@users.noreply.github.com>
2021-11-20 21:23:49 -0500

    Weighted grade averages added
    CHANGES
    _______________________________________________________________________________________________________
    - Took out string conversion and used the .2f format to print averages and for writing to .txt file
    - Calculated Overall grade with weighted totals for better accuracy when calculating grade
    - Shortened displayed numbers to the hundredth decimal place for better readability

--- Grader_TaylorBrown/Grader.py
+++ Grader_TaylorBrown/Grader.py
@@ -3,6 +3,15 @@
 # This program takes in grades from txt files and calculates you actuall grade
 
 
+"""
+CHANGES
+_____________________________________________________________
+- Took out string conversion and used the .2f format to print averages and for writing to .txt file
+- Calculated Overall grade with weighted totals for better accuracy when calculating grade
+- Shortened displayed numbers to the hundredth decimal place for better readability
+"""
+
+
 def main():
 # Declares all category lists for files to write to
     LTL = []
@@ -79,66 +88,45 @@ def main():
     SAM_TEXT_averages = cal_average(i_SAM_TEXT)
     SAM_EXAM_averages = cal_average(i_SAM_EXAM)
     TEST_averages = cal_average(i_TEST)
+   
+    
+# Gets the weighted total for each grade (Weighted Average of Learner to Learner Assignments(10.00%), 
+# Quizzes(20.00%), SAM Projects(20.00%), SAM Capstone Projects(10.00%), SAM Textbook Projects(10.00%), SAM Exams(15.00%), Tests(15.00%))
+    W_LTL_averages = LTL_averages            * .1  #10%
+    W_QUIZ_averages = QUIZ_averages          * .2  #20%
+    W_SAM_PROJ_averages = SAM_PROJ_averages  * .2  #20%
+    W_SAM_CAP_averages = SAM_CAP_averages    * .1  #10%
+    W_SAM_TEXT_averages = SAM_TEXT_averages  * .1  #10%
+    W_SAM_EXAM_averages = SAM_EXAM_averages  * .15 #15%
+    W_TEST_averages = TEST_averages          * .15 #15%
     
     
-# Apperds each average to overall
-    overall = [ LTL_averages,
-                QUIZ_averages,
-                SAM_PROJ_averages,
-                SAM_CAP_averages,
-                SAM_TEXT_averages,
-                SAM_EXAM_averages,
-                TEST_averages]
+# Calculates the overall grade with the weighted totals
+    OVERALL_averages = W_LTL_averages + W_QUIZ_averages + W_SAM_PROJ_averages + W_SAM_CAP_averages + W_SAM_TEXT_averages + W_SAM_EXAM_averages + W_TEST_averages
     
-    
-# Calculates the overall grade
-    OVERALL_averages = cal_average(overall)
-    
-    
-# Changes back to str so it can write to file 
-    S_LTL_averages = change_to_str(LTL_averages)
-    S_QUIZ_averages = change_to_str(QUIZ_averages)
-    S_SAM_PROJ_averages = change_to_str(SAM_PROJ_averages)
-    S_SAM_CAP_averages = change_to_str(SAM_CAP_averages)
-    S_SAM_TEXT_averages = change_to_str(SAM_TEXT_averages)
-    S_SAM_EXAM_averages = change_to_str(SAM_EXAM_averages)
-    S_TEST_averages = change_to_str(TEST_averages)
-    S_OVERALL_averages = change_to_str(OVERALL_averages)
-    
-    
-# Displays grade average for each category
-    print("L2L",S_LTL_averages)
-    print("Quiz",S_QUIZ_averages)
-    print("SAM Proj",S_SAM_PROJ_averages)
-    print("SAM Cap",S_SAM_CAP_averages)
-    print("SAM Text",S_SAM_TEXT_averages)
-    print("SAM Exam",S_SAM_EXAM_averages)
-    print("Test",S_TEST_averages)
-    print("OVERALL",S_OVERALL_averages)
 
+# Displays grade average for each category
+    print("L2L {0:.2f}".format(LTL_averages))
+    print("Quiz {0:.2f}".format(QUIZ_averages))
+    print("SAM Proj {0:.2f}".format(SAM_PROJ_averages))
+    print("SAM Cap {0:.2f}".format(SAM_CAP_averages))
+    print("SAM Text {0:.2f}".format(SAM_TEXT_averages))
+    print("SAM Exam {0:.2f}".format(SAM_EXAM_averages))
+    print("Test {0:.2f}".format(TEST_averages))
+    print("OVERALL {0:.2f}".format(OVERALL_averages))
+    
 
 # Creates a new files named grades.txt with all the print outs displayed
     with open("grades.txt","w+") as f:
-        f.write("L2L" + "      " + S_LTL_averages + '\n')
-        f.write("Quiz" + "     " + S_QUIZ_averages + '\n')
-        f.write("SAM Proj" + " " + S_SAM_PROJ_averages + '\n')
-        f.write("SAM Cap" + "  " + S_SAM_CAP_averages + '\n')
-        f.write("SAM Text" + " " + S_SAM_TEXT_averages + '\n')
-        f.write("SAM Exam" + " " + S_SAM_EXAM_averages + '\n')
-        f.write("Test" + "     " + S_TEST_averages + '\n')
-        f.write("OVERALL" + "  " + S_OVERALL_averages + '\n')
-        
-            
-            
-def change_to_str(var):
-    """
-        Takes the averages and changes it back to a string so the program can write to grades.txt
-    """
-    temp_var = ''
-    temp_var += str(var)
-    return temp_var
-    
-    
+        f.write("L2L" + "      " + "{0:.2f}".format(LTL_averages) + '\n')
+        f.write("Quiz" + "     " + "{0:.2f}".format(QUIZ_averages) + '\n')
+        f.write("SAM Proj" + " " + "{0:.2f}".format(SAM_PROJ_averages) + '\n')
+        f.write("SAM Cap" + "  " + "{0:.2f}".format(SAM_CAP_averages) + '\n')
+        f.write("SAM Text" + " " + "{0:.2f}".format(SAM_TEXT_averages) + '\n')
+        f.write("SAM Exam" + " " + "{0:.2f}".format(SAM_EXAM_averages) + '\n')
+        f.write("Test" + "     " + "{0:.2f}".format(TEST_averages) + '\n')
+        f.write("OVERALL" + "  " + "{0:.2f}".format(OVERALL_averages) + '\n')
+                  
             
 def change_to_int(lst):
     """
