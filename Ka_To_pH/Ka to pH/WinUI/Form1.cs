fd8298c016d3a54e7c519f124f4d4b81617d0f83
Taylor Brown <78773029+TaylorBrown96@users.noreply.github.com>
2023-08-02 03:40:53 -0400

    Add files via upload

--- Ka_To_pH/Ka to pH/WinUI/Form1.cs
+++ Ka_To_pH/Ka to pH/WinUI/Form1.cs
@@ -14,24 +14,36 @@ namespace WinUI
         {
             try
             {
-                
-                    
+                // Makes sure that the user inputed values for each numerical field.
+                if (tb_KaDecimal.Text.Length == 0 || tb_KaPower.Text.Length == 0 || tb_MOfSolution.Text.Length == 0)
+                {
+                    throw new Exception();
+                }
+
+                // Appends user data to newly created variables.
                 double KaDecimal = Convert.ToDouble(tb_KaDecimal.Text);
                 double KaPower = Convert.ToDouble(tb_KaPower.Text);
                 double MOfSolution = Convert.ToDouble(tb_MOfSolution.Text);
-                double x = Math.Sqrt(KaDecimal * Math.Pow(10, KaPower) * MOfSolution);
 
-                double pH = -Math.Log10(x);
-                double ionization = x / MOfSolution * 100;
+                // Performs apprpriate math with values given to attain (value of X, pH, and ionization). 
+                double X = Math.Sqrt(KaDecimal * Math.Pow(10, KaPower) * MOfSolution);
+                double pH = -Math.Log10(X);
+                double ionization = X / MOfSolution * 100;
 
-                MessageBox.Show("The value of x is: " + string.Format("{0:N8}", x) +
+                // Retrives the solution name. If no name is given the default is set to "unknown".
+                string solutionName = "unknown";
+                if (tb_SolutionName.Text.Length > 0) {solutionName = tb_SolutionName.Text;}
+
+                // Displays the calculations performed.
+                MessageBox.Show("The value of X is: " + string.Format("{0:N8}", X) +
                             "\n\nThe pH is: " + string.Format("{0:N2}", pH) +
                             "\n\n% ionization: " + string.Format("{0:N2}%", ionization)
-                            , "Solution for " + tb_SolutionName.Text);
+                            , "Solution for " + solutionName);
             }
             catch
             {
-                MessageBox.Show("Please enter valid numerical values!", "Error!");
+                // Prompts the user to check their inputs.
+                MessageBox.Show("Please make sure all fields have valid numerical values!", "Error!");
             }
         }
     }
